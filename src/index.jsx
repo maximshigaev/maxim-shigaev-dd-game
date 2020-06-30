62cf534a542903eb341492f9a6bfdfdd254e807a
Maxim <financial_monster@mail.ru>
2020-06-30 17:02:31 +0400

    Add variables.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './reset.scss';
+import './variables.scss';
 import './scaffolding.scss';
 import { App } from './components/app/App';
 
