c97f3d563a759b5b8bcadbb5a86903f920bd74ad
Maxim <financial_monster@mail.ru>
2020-07-03 17:14:02 +0400

    Remove variables.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './reset.scss';
-import './variables.scss';
 import './scaffolding.scss';
 import { App } from './components/app/App';
 
