310df164dbcb5f719595c9001becc2bf062f52bc
Maxim <financial_monster@mail.ru>
2020-07-08 09:15:44 +0400

    Refactoring of the updating logic in helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -6,140 +6,50 @@ const getRandomInteger = (min, max) => {
   return Math.floor(rand);
 }
 
-const updateHealth = (bars, action) => {
-    const health = bars.find((bar) => bar.title === `Здоровье`);
-
-    switch(action) {
-        case `Есть`:
-            return {
-                ...health,
-                quantity: getRandomBoolean()
-                    ? Math.min(health.quantity + 2, 100)
-                    : Math.max(health.quantity - 2, 0)
-            }
-
-        case `Пить`:
-            return {
-                ...health,
-                quantity: getRandomBoolean()
-                    ? Math.min(health.quantity + 1, 100)
-                    : Math.max(health.quantity - 1, 0)
-            }
-
-        case `Отдохнуть`:
-            return {
-                ...health,
-                quantity: Math.min(health.quantity + getRandomInteger(1, 10), 100)
-            }
-
-        default:
-            return health;
+const mapActionToBar = {
+    Есть: {
+        Здоровье: () => getRandomBoolean() ? 2 : -2,
+        Голод: () => -10
+    },
+    Пить: {
+        Здоровье: () => getRandomBoolean() ? 1 : -1,
+        Жажда: () => -10
+    },
+    Отдохнуть: {
+        Здоровье: () => getRandomInteger(1, 10),
+        Жажда: () => -getRandomInteger(1, 100),
+        Голод: () => -getRandomInteger(1, 100),
+        Усталость: () => -10
+    },
+    Работать: {
+        Жажда: () => getRandomInteger(30, 40),
+        Голод: () => getRandomInteger(10, 20),
+        Усталость: () => 10
     }
 }
 
-const updateThirst = (bars, action) => {
-    const thirst = bars.find((bar) => bar.title === `Жажда`);
-
-    switch(action) {
-        case `Пить`:
-            return {
-                ...thirst,
-                quantity: Math.max(thirst.quantity - 10, 0)
-            }
-
-        case `Работать`:
-            return {
-                ...thirst,
-                quantity: Math.min(thirst.quantity + getRandomInteger(30, 40), 100)
-            }
-
-        case `Отдохнуть`:
-            return {
-                ...thirst,
-                quantity: Math.max(thirst.quantity - getRandomInteger(1, 100), 0)
-            }
-
-        default:
-            return thirst;
-    }
-}
-
-const updateHunger = (bars, action) => {
-    const hunger = bars.find((bar) => bar.title === `Голод`);
-
-    switch(action) {
-        case `Есть`:
-            return {
-                ...hunger,
-                quantity: Math.max(hunger.quantity - 10, 0)
-            }
-
-        case `Работать`:
-            return {
-                ...hunger,
-                quantity: Math.min(hunger.quantity + getRandomInteger(10, 20), 100)
-            }
-
-        case `Отдохнуть`:
-            return {
-                ...hunger,
-                quantity: Math.max(hunger.quantity - getRandomInteger(1, 100), 0)
-            }
-
-        default:
-            return hunger;
-    }
-}
-
-const updateFatigue = (bars, action) => {
-    const fatigue = bars.find((bar) => bar.title === `Усталость`);
-
-    switch(action) {
-        case `Работать`:
-            return {
-                ...fatigue,
-                quantity: Math.min(fatigue.quantity + 10, 100)
-            }
-
-        case `Отдохнуть`:
-            return {
-                ...fatigue,
-                quantity: Math.max(fatigue.quantity - 10, 0)
-            }
-
-        default:
-            return fatigue;
+const updateBar = (title, bars, action) => {
+    const currentBar = bars.find((bar) => bar.title === title);
+    const quantityDiff =  mapActionToBar[action.title][title]();    
+    const newQuantity = (quantityDiff > 0)
+        ? Math.min(currentBar.quantity + quantityDiff, 100)
+        : Math.max(currentBar.quantity + quantityDiff, 0);
+        
+    return {
+        ...currentBar,
+        quantity: newQuantity
     }
 }
 
 export const updateBars = (bars, action) => {
-    const map = {
-        Здоровье: {
-            updater: updateHealth,
-            actions: [`Есть`, `Пить`, `Отдохнуть`]
-        },
-        Жажда: {
-            updater: updateThirst,
-            actions: [`Пить`, `Работать`, `Отдохнуть`]
-        },
-        Голод: {
-            updater: updateHunger,
-            actions: [`Есть`, `Работать`, `Отдохнуть`]
-        },
-        Усталость: {
-            updater: updateFatigue,
-            actions: [`Работать`, `Отдохнуть`]
-        }
-    }
     const updatedBars = [];
 
     bars.forEach((bar) => {      
-        const isCurrentlyChanged = map[bar.title]
-            .actions
-            .includes(action);
+        const isCurrentlyChanged = Object.keys(mapActionToBar[action.title])
+            .includes(bar.title);
 
         if (isCurrentlyChanged) {
-            updatedBars.push(map[bar.title].updater(bars, action));
+            updatedBars.push(updateBar(bar.title, bars, action));
         } else {
             updatedBars.push(bar);
         }
