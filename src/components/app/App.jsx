d1053c78000827aac812e4b75aea5f88e3f74287
Maxim <financial_monster@mail.ru>
2020-07-08 07:43:18 +0400

    Add routing to the project

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { MainPage } from '../../pages/mainPage/MainPage.jsx';
+import { Routes } from '../../constants.js';
 
 import './App.scss';
 
 const App = () => (
-    <>
-        <MainPage />
-    </>
+    <Switch>
+        <Route path={`${process.env.PUBLIC_URL}${Routes.MAIN_PAGE}`} component={MainPage} exact />
+        <Redirect to={`${process.env.PUBLIC_URL}${Routes.MAIN_PAGE}`} />
+    </Switch>
 );
 
 export { App };
