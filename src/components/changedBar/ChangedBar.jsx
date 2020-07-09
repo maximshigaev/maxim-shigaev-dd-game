7dc3be0bfe212c5025a4a745bd4898a589a09863
Maxim <financial_monster@mail.ru>
2020-07-09 10:48:02 +0400

    Add the ChangedBar component

---
+++ src/components/changedBar/ChangedBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ChangedBar = ({changedBar}) => (
+    <tr className="row">
+        <td className="cell cell--little">{changedBar.barTitle}</td>
+        <td className="cell cell--medium">{changedBar.diff}</td>
+        <td className="cell cell--big">{changedBar.quantity}</td>
+    </tr>
+);
+
+ChangedBar.propTypes = {
+    changedBar: PropTypes.exact({
+        barTitle: PropTypes.string.isRequired,
+        diff: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired
+    })
+}
+
+export { ChangedBar };
